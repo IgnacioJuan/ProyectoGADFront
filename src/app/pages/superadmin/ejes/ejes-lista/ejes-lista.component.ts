8f84402351061ddde760f2e0f9e31c8e7b0f19bd
CAB3ZON <marcospillcocontreras@gmail.com>
2023-08-28 08:27:06 -0500

    Actualizacion de ejes funcional 1era parte
    Marcos Pillco

--- src/app/pages/superadmin/ejes/ejes-lista/ejes-lista.component.ts
+++ src/app/pages/superadmin/ejes/ejes-lista/ejes-lista.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {EjeService} from "../../../../services/eje.service";
+import {Eje} from "../../../../models/eje";
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-ejes-lista',
   templateUrl: './ejes-lista.component.html',
   styleUrls: ['./ejes-lista.component.css']
 })
-export class EjesListaComponent {
+export class EjesListaComponent implements OnInit{
+  ejeobjeto: Eje = new Eje();
+  ejeslista: Eje[] = [];
+  mostrarFormulario: boolean = false;
+  nombreeje = '';
+  constructor(private ejeServicio:EjeService) { }
+
+  //aqui esta el on init para cosas que se carguen al iniciar el programa
+  ngOnInit(): void {
+    this.obtenerejes();
+  }
+
+  private obtenerejes(){
+    this.ejeServicio.obtenerListaejes().subscribe(dato => {
+      this.ejeslista = dato;
+    });
+  }
+guardareje(){
+    console.log(this.ejeobjeto);
+
+}
+  guardarDatos(formulario: any) {
+    // Aquí puedes procesar y guardar los datos ingresados en el formulario
+    // formulario.value contendrá los valores ingresados
+    console.log(formulario.value);
+    // Puedes realizar más acciones como enviar datos al servidor, etc.
+  }
+
+  toggleFormulario() {
+    this.mostrarFormulario = !this.mostrarFormulario; // Cambia el valor para mostrar u ocultar el formulario
+  }
 
 }
--- src/app/services/eje.service.ts
+++ src/app/services/eje.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import baserUrl from './helper';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Eje} from "../models/eje";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EjeService {
 
-  constructor() { }
+  constructor(private httpClient : HttpClient) { }
+
+  obtenerListaejes(): Observable<Eje[]>{
+    return this.httpClient.get<Eje[]>(`${baserUrl}/api/eje/listar`);
+  }
+
+  crearejes(r: Eje): Observable<Object> {
+    return this.httpClient.post(`${baserUrl}/api/eje/crear`, r);
+  }
+
+
 }
