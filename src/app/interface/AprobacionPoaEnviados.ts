b0ce64ea8af121951ead84804b999a0e4c73c0ff
dianasaico20 <diana.saico.est@tecazuay.edu.ec>
2023-09-06 02:07:30 -0500

    Fix filtro de poas enviados por admin

---
+++ src/app/interface/AprobacionPoaEnviados.ts
@@ -0,0 +1,7 @@
+export interface AprobacionPoasEnviadosProjection{
+    observacion: string;
+    estado: string;
+    id_aprobacionpoa: number;
+    primer_nombre: string;
+    primer_apellido: string;
+}
