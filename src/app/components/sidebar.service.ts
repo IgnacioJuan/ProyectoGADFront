929d3f1eee0e718d2765499fc13a27f0ef76e133
IgnacioJuan <juan.guachichullca.est@tecazuay.edu.ec>
2023-09-23 21:01:42 -0500

    slide

--- src/app/components/sidebar.service.ts
+++ src/app/components/sidebar.service.ts
@@ -12,39 +12,28 @@ export class SidebarService {
       titulo: "POA",
       submenu: [
         { titulo: 'Creacion', url: 'adm/poa/registrarpoa', icono: 'fas fa-check-square' },
+        { titulo: 'Estados', url: 'adm/poasEnviadosAdmin', icono: 'fas fa-cubes' },
         { titulo: 'Avance Meta', url: 'adm/evaluar-poa/poas', icono: 'fas fa-list-ul ' },
-        { titulo: 'Presupuesto Ejecutado', url: 'adm/presup-ejecut/tabla-poas', icono: 'fas fa-list-ul ' },
       ]
     },
+
     {
       icono: 'nav-icon fas fa-users',
       titulo: "Responsables",
       submenu: [
-        { titulo: 'Listado Responsables', url: 'adm/create-responsables', icono: 'fas fa-check-square' },
-        { titulo: 'Asignacion', url: 'adm/asignacion-actividades/poa-actividad', icono: 'fas fa-check-square' },
-
-
+        { titulo: 'Control Responsables', url: 'adm/create-responsables', icono: 'fas fa-check-square' },
+        { titulo: 'Asignacion ', url: 'adm/asignacion-actividades/poa-actividad', icono: 'fas fa-check-square' },
       ]
     },
-    {
-      icono: 'nav-icon fas fa-file-signature',
-      titulo: "Poas Enviados",
-      submenu: [
-        { titulo: 'Listado de Poas', url: 'adm/poasEnviadosAdmin', icono: 'fas fa-cubes' },
-      ]
-    },
+
     {
       icono: 'nav-icon fas fa-file-invoice',
-      titulo: "Solicitud de Presupuesto",
+      titulo: "Presupuesto",
       submenu: [
-        { titulo: 'Listado de Solicitudes', url: 'adm/poas-solicitudes/listarPoasSoli', icono: 'fas fa-list-ul ' },
-      ]
-    },
-    {
-      icono: 'nav-icon fas fa-file-invoice',
-      titulo: "Actividades",
-      submenu: [
+        { titulo: 'Solicitudes', url: 'adm/poas-solicitudes/listarPoasSoli', icono: 'fas fa-list-ul ' },
         { titulo: 'Financiamiento', url: 'sup/actividades-presupuestos/tabla-poas', icono: 'fas fa-list-ul ' },
+        { titulo: 'Ejecucion Periodo', url: 'adm/presup-ejecut/tabla-poas', icono: 'fas fa-list-ul ' },
+
       ]
     },
     {
@@ -63,7 +52,7 @@ export class SidebarService {
       icono: 'nav-icon fas   fa-users',
       titulo: "Usuarios",
       submenu: [
-        { titulo: 'Lista de Usuarios', url: 'sup/usuarios', icono: 'fas fa-list-ul ' },
+        { titulo: 'Control Usuarios', url: 'sup/usuarios', icono: 'fas fa-list-ul ' },
       ]
     },
     {
@@ -76,8 +65,6 @@ export class SidebarService {
         { titulo: 'Objetivos PND', url: 'sup/ejes/ejestabla', icono: 'fas fa-list-ul ' },
         { titulo: 'Competencia', url: 'sup/crearcompe', icono: 'fas fa-list-ul ' },
         { titulo: 'ODS', url: 'sup/objetivoods-lista', icono: 'fas fa-list-ul ' },
-
-
       ]
     },
 
@@ -92,23 +79,23 @@ export class SidebarService {
 
       ]
     },
-    {
-      icono: 'nav-icon fas fa-file-invoice',
-      titulo: "Evidencias",
-      submenu: [
-        { titulo: 'Estados', url: 'sup/resumen-evidencias-responsable/evidencias', icono: 'fas fa-list-ul ' },
-
-      ]
-    },
+    
 
     {
       icono: 'nav-icon fas fa-solid fa-list',
       titulo: "Actividades",
       submenu: [
         { titulo: 'Financiamiento', url: 'sup/actividades-presupuestos/tabla-poas', icono: 'fas fa-list-ul ' },
-        { titulo: 'Evidencias Rechazadas', url: 'sup/archivos-rechazados/Actividades_Evi_Rechazados', icono: 'fas fa-list-ul ' },
-        { titulo: 'Lista de Actividades', url: 'sup/usuario-actividades/listar-actividades', icono: 'fas fa-list-ul ' },
-        { titulo: 'Presupuesto Ejecutado', url: 'adm/presup-ejecut/tabla-poas', icono: 'fas fa-list-ul ' },
+        { titulo: 'Asignaciones', url: 'sup/usuario-actividades/listar-actividades', icono: 'fas fa-list-ul ' },
+
+      ]
+    },
+    {
+      icono: 'nav-icon fas fa-file-invoice',
+      titulo: "Evidencias",
+      submenu: [
+        { titulo: 'Estados', url: 'sup/resumen-evidencias-responsable/evidencias', icono: 'fas fa-list-ul ' },
+        //{ titulo: 'Evidencias Rechazadas', url: 'sup/archivos-rechazados/Actividades_Evi_Rechazados', icono: 'fas fa-list-ul ' },
 
       ]
     },
@@ -120,14 +107,7 @@ export class SidebarService {
         { titulo: 'Aprobación de Actividades', url: 'sup/aprobar-actividades/poa-actividad', icono: 'fas fa-list-ul ' },
       ]
     },
-    // {
-    //   icono: 'nav-icon fas fa-solid fa-list',
-    //   titulo: "Reportes",
-    //   submenu: [
-    //     { titulo: 'Reporte Poa', url: 'sup/reportePoa', icono: 'fas fa-list-ul ' },
 
-    //   ]
-    // },
 
     {
       icono: 'nav-icon fas fa-file-invoice',
@@ -135,7 +115,9 @@ export class SidebarService {
       submenu: [
         { titulo: 'Metas', url: 'repor/reporte_metas', icono: 'fas fa-list-ul ' },
         { titulo: 'Competencia', url: 'repor/reporteEspecificoCompetencia', icono: 'fas fa-list-ul ' },
-        { titulo: 'Presupuestos', url: 'sup/reporte-presupuesto/reporteproyecto', icono: 'fas fa-list-ul ' }
+        { titulo: 'Presupuestos', url: 'sup/reporte-presupuesto/reporteproyecto', icono: 'fas fa-list-ul ' },
+        { titulo: 'Periodos', url: 'adm/presup-ejecut/tabla-poas', icono: 'fas fa-list-ul ' },
+
       ]
     },
 
